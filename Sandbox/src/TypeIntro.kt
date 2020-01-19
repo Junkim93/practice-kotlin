374863e9ec74cf74274d3e2d5482b9d9429a7f31
Jun Kim <93kimhyunjun@gmail.com>
2020-01-20 01:31:36 +0900

    init project

---
+++ Sandbox/src/TypeIntro.kt
@@ -0,0 +1,37 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main() {
+//    var experiencePoints = 5
+//    var experiencePointsMissMatch: Int = "thirty-two"
+
+//    val playerName = "Notion"
+//    playerName = "Notice"
+    // 재할당할 수 없음을 나타내는 에러 메세지가 나타난다.
+
+//    experiencePoints += 5
+//    status test
+//    println(playerName)
+//    println(experiencePoints)
+//    println(MAX_EXPERIENCE)
+//    println(experiencePointsMissMatch)
+
+    var playerName = "에스트라곤"
+    var playerCoin = 5000
+
+    val hasSteed = "Not obtained"
+
+    println("player: $playerName")
+    println(playerName.reversed())
+    println(hasSteed)
+
+    val storeName = "The Unicorn Horn"
+    val storeMasterName = "Jain"
+    var menuList = listOf("soju", "beer", "soda")
+}
+
+/**
+1. Int 타입으로 experiencePoints 라는 변수를 지정
+2. 대입 연산자를 통해 정수값 5를 지정
+
+1. 지정한 타입 값과 다를 경우 에러가 발생한다.
+**/
